61e7e5eaecd56d8777434442d8ae0e37d4ea7935
AkioShigekane <motorado519+git@gmail.com>
2017-02-28 12:27:47 +0900

    ファイルを整理

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Self driving wheel chair robot. Planed to merge to https://github.com/irvs/ros_tms in the futere.
 
 **僕の考えたさいきょうのみまもる君**
+
+[実機実験手順](./doc/実機実験手順.md)
--- move_base_outdoor.sh
+++
@@ -1,6 +0,0 @@
-roscore
-roslaunch tms_rc_mimamorukun_control minimal_with_tf.launch
-rviz -d `rospack find chairbot_selfdrive`/config/ndt_macher.rviz
-roslaunch ndt_localizer velodyne_matcher.launch
-rsolaunch chairbot_selfdrive pose2frame.launch
-roslaunch tms_rc_mimamorukun_control keyop.launch
